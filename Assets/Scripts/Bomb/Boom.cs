7ac689eb37a662c36d61388cd67a8b0b99b9b8bb
creativeanya <106864797+creativeanya@users.noreply.github.com>
2024-05-05 15:13:29 +0300

    Новая туррель бомба с таймером

---
+++ Assets/Scripts/Bomb/Boom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boom : MonoBehaviour
+
+{
+    public float radius = 5f; // Радиус области вокруг бомбы, в котором будут уничтожаться объекты
+    public float timing = 5f;
+    void Start()
+    {
+        Invoke("Explode", timing); // Вызываем метод Explode через 2 секунды
+    }
+
+    void Explode()
+    {
+        // Получаем все коллайдеры в определенном радиусе вокруг бомбы
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        // Уничтожаем все объекты с тегом "Enemy" в радиусе действия
+        foreach (Collider hit in colliders)
+        {
+            if (hit.CompareTag("Enemy"))
+            {
+                Destroy(hit.gameObject);
+            }
+        }
+
+        // Уничтожаем саму бомбу
+        Destroy(gameObject);
+    }
+}
+
