a5684c1338cca21d4581a34b6f2a3bf156bcce89
GarinIvan <ivangarin08@gmail.com>
2024-05-11 18:57:22 +0300

    Очень крутой коммит
    Тут я провёл просто огромную работу над фонами уровней, доработал механики многих турелей, исправил баги, улучшил систему сохранений.

--- Assets/Scripts/CardGun/TurretDestroyer.cs
+++ Assets/Scripts/CardGun/TurretDestroyer.cs
@@ -6,10 +6,26 @@ public class TurretDestroyer : MonoBehaviour
 {
     public float lifeTime;
     public ParticleSystem boom;
+    public Building color;
+    private bool isBuildingReady = false;
     private void Start()
     {
-        Invoke("DestroyTurret", lifeTime);
+        color = GetComponent<Building>();
+        color.SetColor(true);
+        isBuildingReady = true;
     }
+
+    private void FixedUpdate()
+    {
+        if (isBuildingReady)
+        {
+            if (color._renderer.material.color == color.colorMaterial)
+            {
+                Invoke("DestroyTurret", lifeTime);
+            }
+        }
+    }
+
     void DestroyTurret()
     {
         Instantiate(boom, transform.position, transform.rotation);
